fc794214b967b36baaf0e6e88acff579dce024e8
Philip Adeoye <semini08@gmail.com>
2021-01-20 17:17:33 -0500

    attempt to solve pwa issues

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
-import './responsive.css';
-import './typography.css';
-import App from './app/App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom';
-import ScrollToTop from './app/ScrollToTop';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import "./index.css";
+import "./responsive.css";
+import "./typography.css";
+import App from "./app/App";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter } from "react-router-dom";
+import ScrollToTop from "./app/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <ScrollToTop />
         <App />
       </HashRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
